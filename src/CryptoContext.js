233f5d9de411931218eaba640c68bf0419fdaf87
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-23 12:44:26 +0530

    updated

---
+++ src/CryptoContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+    const [currency, setCurrency] = useState("INR");
+    const [symbol, setSymbol] = useState("₹");
+
+    useEffect(() => {
+        if (currency === "INR") setSymbol("₹");
+        else if (currency === "USD") setSymbol("$");
+        else if (currency === "EUR") setSymbol("€");
+        else if (currency === "GBP") setSymbol("£");
+        else if (currency === "AUD") setSymbol("a$");
+    }, [currency]);
+
+    return (
+        <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+            {children}
+        </Crypto.Provider>
+    );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+    return useContext(Crypto);
+};
